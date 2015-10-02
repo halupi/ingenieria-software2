ec2a23d403bc73b6d1de6b5d8cd20502bf3061c5
Jorge Ramón <jorge.ramon@Miausi-Mac.local>
2015-10-02 00:28:54 -0500

    Actualizando colección de CV

--- app/schemas/cv.js
+++ app/schemas/cv.js
@@ -1,5 +1,31 @@
 var CVSchema = new Schema({
-	name: { type: String }
+	name: { type: String, required: true },
+	born: { type: String, required: true },
+	phone: { type: String, required: true },
+	email: { type: String, required: true },
+	show_email: { type: Boolean, required: true, default: false },
+	address: { type: String },
+	rfc: { type: String, required: true },
+	specialization: [{ type: String }],
+	grade: {
+		name: { type: String, required: true },
+		specialty: { type: String, required: true },
+		year: { type: Number, required: true },
+		entity: { type: String , required: true}
+	},
+	teaching_experience: { type: String, required: true },
+	charges: [{ type: String }],
+	reviser: { type: String, required: true },
+	publications: [{ type: String }],
+	conferences: [{ type: String }],
+	courses: [{ type: String }],
+	human_resources: {
+		thesis: { type: String, required: true },
+		advices: { type: String, required: true },
+		tutorials: { type: String, required: true }
+	},
+	academic_entity: { type: String, required: true },
+	area_experience: { type: String, required: true }
 });
 
 module.exports = CVSchema;
